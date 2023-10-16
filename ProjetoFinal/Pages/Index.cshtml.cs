095d04ccc938707c3a6f6e89cff53bc3603e24ca
Lissa Takahashi <99764365+lissatakahashi@users.noreply.github.com>
2023-10-16 16:53:02 -0300

    foi aplicado bootstrap

--- ProjetoFinal/Pages/Index.cshtml.cs
+++ ProjetoFinal/Pages/Index.cshtml.cs
@@ -3,23 +3,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace ProjetoFinal.Pages;
 public class Index : PageModel
 {
-    // private readonly ILogger<Index> _logger;
-
-    // public Index(ILogger<Index> logger)
-    // public Index()
-    // {
-    //     // _logger = logger;
-    // }
+    public Index()
+    {
+        
+    }
 
     public void OnGet()
     {
         
     }
 }
-
-public record AlunoModel(
-    int? Id,
-    string Nome,
-    DateTime DataNascimento,
-    string Telefone
-);
